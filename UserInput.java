f6d3980b0149ea7137ac437676afff07ba5d8198
Hirudhanya <hirudhanya985@gmail.com>
2026-08-06 15:11:01 +0530

    feat:add userinput

--- UserInput.java
+++ UserInput.java
@@ -1,14 +1,20 @@
-class UserInput{
-    public static void main(string[] args){
+class UserInput {
+    public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         System.out. println(x:"Enter the name:")
         String name = in.nextLine();
-        System.out.println(x:"Enter your Register number:")
-        int register_number = in.nextInt();
         System.out.println(x:"Enter your Department:")
         String Department= in.nextLine();
         System.out.println(x:"Enter your CGPA:")
-        Float CGPA = in.nextFloat();
+        Float CGPA = in.nextfloat();
+        System.out.println(x:"Enter your Register number:")
+        Long register_number = in.nextLong();
+        in.close()
 
+        System.out.println("Name:"+name)
+        System.out.println("Register_name:"+register_number)
+        System.out.println("Department:"+Department)
+        System.out.println("CGPA:"+CGPA)
+        
     }
 }
