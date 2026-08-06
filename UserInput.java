2bd5a1c31357163a2805853c43ca7650c1aaf64a
Hiru-KreupAI <hirudhanya@daftaracc.com>
2026-08-06 14:21:01 +0530

    first commit

---
+++ UserInput.java
@@ -0,0 +1,14 @@
+class UserInput{
+    public static void main(string[] args){
+        Scanner in = new Scanner(System.in);
+        System.out. println(x:"Enter the name:")
+        String name = in.nextLine();
+        System.out.println(x:"Enter your Register number:")
+        int register_number = in.nextInt();
+        System.out.println(x:"Enter your Department:")
+        String Department= in.nextLine();
+        System.out.println(x:"Enter your CGPA:")
+        Float CGPA = in.nextFloat();
+
+    }
+}
