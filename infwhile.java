b6f1febaf5fc2c0cabeca61134aa2b0cb103323f
Hirudhanya <hirudhanya985@gmail.com>
2026-08-08 12:13:33 +0530

    infinite while loop

---
+++ infwhile.java
@@ -0,0 +1,10 @@
+// infinite while loop ( it shown the much the system can run)
+public class infwhile {
+    public static void main(String[] args) {
+        int i =1;
+        while(true){
+            System.out.println(i+"helloo");
+            i++;
+        }
+    }
+}
