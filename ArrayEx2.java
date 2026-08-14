4f5bc9c0883fcbf340d1197fd6875f794e38cd5e
Hirudhanya <hirudhanya985@gmail.com>
2026-08-14 10:18:21 +0530

    rev array

---
+++ ArrayEx2.java
@@ -0,0 +1,24 @@
+//Reverse a array
+import java.util.Scanner;
+
+public class ArrayEx2 {
+    public static void revArray(int [] nums) {
+        int l = 0;
+        int r = nums.length-1;
+        while(l<r){
+            int temp = nums[l];
+            nums[l] = nums[r];
+            nums[r] = temp;
+            l++; r--;
+        }
+        
+    }
+    public static void main(String[] args) {
+       int nums[] = {2,7,6,3,8,5};
+       revArray(nums);
+       System.out.println("After Reversing: ");
+       for(int i=0; i<nums.length; i++){
+         System.out.print(nums[i]+" ");
+       }
+    }
+}
